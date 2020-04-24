e014b3e63f5b323e0e6ba6036ee92066627ddcee
macarena-chang <changmacarena@gmail.com>
2020-04-24 11:30:39 -0300

    Creo interface INominable

---
+++ Pooflix/src/app/INominable.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.util.List;
+
+public interface INominable {
+    /***************
+     **** METODOS****
+     ***************/
+
+     //Las interfaces No tienen BODY
+     
+
+    boolean ganoPreviamente();
+
+    void reprodducirTrailerNominacion();
+
+    void sacarSelfie(List<Actor> otrosActor);
+
+}
